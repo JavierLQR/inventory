ccca8770d60108fc26ce76a012b7e7fd1c62e5f2
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-17 22:04:56 -0500

    update role.controller.ts

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,17 +1,11 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common'
+import { Controller, Get, Post, Body, VERSION_NEUTRAL } from '@nestjs/common'
 import { RoleService } from './role.service'
 import { CreateRoleDto } from './dto/create-role.dto'
-import { UpdateRoleDto } from './dto/update-role.dto'
 
-@Controller('role')
+@Controller({
+  path: 'roles',
+  version: VERSION_NEUTRAL,
+})
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
@@ -23,20 +17,5 @@ export class RoleController {
   @Get()
   findAll() {
     return this.roleService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roleService.findOne(+id)
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.roleService.update(+id, updateRoleDto)
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roleService.remove(+id)
   }
 }
