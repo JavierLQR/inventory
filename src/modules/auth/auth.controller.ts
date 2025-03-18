b3ca7ca887f8d2eda1180f3a37155ded3350d620
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-18 00:31:38 -0500

    update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res } from '@nestjs/common'
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { CreateAuthDto } from './dto/create-auth.dto'
 import { Response } from 'express'
@@ -8,8 +8,51 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  login(
-    @Body() createAuthDto: CreateAuthDto,
+  async login(
+    @Body() data: CreateAuthDto,
     @Res({ passthrough: true }) res: Response,
-  ) {}
+  ) {
+    const auth = await this.authService.signIn(data)
+    const isProduction = process.env.NODE_ENV === 'production'
+    if (isProduction)
+      // // PRO
+      return res.cookie('auth', auth, {
+        // si pones en true, una vez actualizada la pagina en el front
+        // se pierde las cookies sale undefined
+        // sameSite: isProduction ? 'none' : 'lax', // SameSite=None solo para producción
+        sameSite: 'none', // SameSite=None solo para producción
+        // secure: isProduction, // Secure=true solo en producción (HTTPS)
+        secure: true, // Secure=true solo en producción (HTTPS)
+        httpOnly: true,
+      })
+
+    res.cookie('auth', auth, {
+      sameSite: 'lax',
+      secure: true,
+      httpOnly: true,
+    })
+
+    res.send({ auth, statusCode: HttpStatus.OK })
+  }
+
+  @Get('logout')
+  logout(@Res({ passthrough: true }) res: Response) {
+    try {
+      res.cookie('auth', '', {
+        sameSite: 'lax',
+        secure: true,
+        httpOnly: true,
+        // path: '/',
+      })
+
+      res.send({
+        auth: '',
+        statusCode: HttpStatus.OK,
+        success: true,
+      })
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ message: 'Error logging out' })
+    }
+  }
 }
