97805f18debfb920cb490303632a75c16e3294da
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-20 23:44:39 -0500

    update schema.prisma and product.controller.ts

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -6,10 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
+  ParseUUIDPipe,
 } from '@nestjs/common'
 import { ProductService } from './product.service'
 import { CreateProductDto } from './dto/create-product.dto'
 import { UpdateProductDto } from './dto/update-product.dto'
+import { ListProductDto } from './dto/list-product.dto'
 
 @Controller('product')
 export class ProductController {
@@ -17,26 +20,27 @@ export class ProductController {
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto)
+    return this.productService.createOrUpdate(createProductDto)
   }
 
   @Get()
-  findAll() {
-    return this.productService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id)
+  findAll(@Query() listProductDto: ListProductDto) {
+    return this.productService.findAll(listProductDto)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto)
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productService.createOrUpdate(
+      updateProductDto as CreateProductDto,
+      id,
+    )
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productService.remove(id)
   }
 }
