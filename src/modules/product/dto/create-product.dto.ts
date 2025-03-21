edcf539d395b24d039f3f211e1e9ec0bdcd4fb1d
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-20 23:45:02 -0500

    update create-product.dto.ts

--- src/modules/product/dto/create-product.dto.ts
+++ src/modules/product/dto/create-product.dto.ts
@@ -1,1 +1,23 @@
-export class CreateProductDto {}
+import { Transform } from 'class-transformer'
+import { IsOptional, IsString, IsUUID } from 'class-validator'
+
+export class CreateProductDto {
+  @IsOptional()
+  id?: string
+
+  @Transform(({ value }: { value: string }) => value.toLowerCase().trim())
+  @IsString()
+  name: string
+
+  @IsUUID()
+  @IsString()
+  categoryId: string
+
+  @IsUUID()
+  @IsString()
+  typePresentationId: string
+
+  @IsUUID()
+  @IsString()
+  typeProductId: string
+}
