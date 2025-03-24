62079dc94126449a865d662421230f7659277fe3
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-24 03:19:55 -0500

    update movements.service.ts

--- src/modules/movements/movements.service.ts
+++ src/modules/movements/movements.service.ts
@@ -1,26 +1,63 @@
 import { Injectable } from '@nestjs/common'
-import { CreateMovementDto } from './dto/create-movement.dto'
-import { UpdateMovementDto } from './dto/update-movement.dto'
+import { PrismaService } from 'nestjs-prisma'
 
 @Injectable()
 export class MovementsService {
-  create(createMovementDto: CreateMovementDto) {
-    return 'This action adds a new movement'
-  }
+  constructor(private readonly prismaService: PrismaService) {}
 
-  findAll() {
-    return `This action returns all movements`
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} movement`
-  }
-
-  update(id: number, updateMovementDto: UpdateMovementDto) {
-    return `This action updates a #${id} movement`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} movement`
+  async findAll() {
+    return await this.prismaService.movement.create({
+      data: {
+        balance: 0,
+        category: {
+          connectOrCreate: {
+            create: {
+              name: 'Sin Categoria',
+            },
+            where: {
+              name: 'Sin Categoria',
+            },
+          },
+        },
+        date: new Date(),
+        moventType: {
+          connectOrCreate: {
+            create: {
+              name: 'ENTRADA',
+            },
+            where: {
+              name: 'ENTRADA',
+            },
+          },
+        },
+        product: {
+          connect: {
+            id: '1b8f64c1-2d31-4e04-9916-41018191fe6b',
+          },
+        },
+        entry: 2,
+        exit: 2,
+        TypePresentation: {
+          connectOrCreate: {
+            create: {
+              name: 'presentation',
+            },
+            where: {
+              name: 'presentation',
+            },
+          },
+        },
+        typeProduct: {
+          connectOrCreate: {
+            create: {
+              name: 'nohay',
+            },
+            where: {
+              name: 'nohay',
+            },
+          },
+        },
+      },
+    })
   }
 }
