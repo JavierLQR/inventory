94e59c634413ba13cb069d7a03e5f43820f53b14
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-24 03:19:43 -0500

    update movements.controller.ts

--- src/modules/movements/movements.controller.ts
+++ src/modules/movements/movements.controller.ts
@@ -15,31 +15,8 @@ import { UpdateMovementDto } from './dto/update-movement.dto'
 export class MovementsController {
   constructor(private readonly movementsService: MovementsService) {}
 
-  @Post()
-  create(@Body() createMovementDto: CreateMovementDto) {
-    return this.movementsService.create(createMovementDto)
-  }
-
   @Get()
   findAll() {
     return this.movementsService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.movementsService.findOne(+id)
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateMovementDto: UpdateMovementDto,
-  ) {
-    return this.movementsService.update(+id, updateMovementDto)
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.movementsService.remove(+id)
   }
 }
