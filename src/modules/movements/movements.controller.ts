2746173e1c6b4930ae6dd2ce3bf3fdf426b58f56
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-26 00:17:06 -0500

    update movements.controller.ts

--- src/modules/movements/movements.controller.ts
+++ src/modules/movements/movements.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common'
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common'
 import { MovementsService } from './movements.service'
 import { ListMovemtsDto } from './dto/list-movemt.dto'
+import { CreateMovementDto } from './dto/create-movement.dto'
+import { AuthUserGuard } from '../auth/guards/auth.guard'
 
 @Controller('movements')
+@UseGuards(AuthUserGuard)
 export class MovementsController {
   constructor(private readonly movementsService: MovementsService) {}
 
   @Get()
   findAll(@Query() listMovemtsDto: ListMovemtsDto) {
     return this.movementsService.findAll(listMovemtsDto)
+  }
+  @Post()
+  create(@Body() createMovementDto: CreateMovementDto) {
+    return this.movementsService.create(createMovementDto)
   }
 }
