d682164c6ce47dd69010e9b20ba7973153236e41
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-25 23:37:41 -0500

    update 3 files, create 1 file and delete 1 file

--- src/modules/movements/movements.controller.ts
+++ src/modules/movements/movements.controller.ts
@@ -1,22 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common'
+import { Controller, Get, Query } from '@nestjs/common'
 import { MovementsService } from './movements.service'
-import { CreateMovementDto } from './dto/create-movement.dto'
-import { UpdateMovementDto } from './dto/update-movement.dto'
+import { ListMovemtsDto } from './dto/list-movemt.dto'
 
 @Controller('movements')
 export class MovementsController {
   constructor(private readonly movementsService: MovementsService) {}
 
   @Get()
-  findAll() {
-    return this.movementsService.findAll()
+  findAll(@Query() listMovemtsDto: ListMovemtsDto) {
+    return this.movementsService.findAll(listMovemtsDto)
   }
 }
--- src/modules/movements/movements.service.ts
+++ src/modules/movements/movements.service.ts
@@ -1,63 +1,71 @@
-import { Injectable } from '@nestjs/common'
+import { HttpStatus, Injectable } from '@nestjs/common'
 import { PrismaService } from 'nestjs-prisma'
+import { ListMovemtsDto } from './dto/list-movemt.dto'
 
 @Injectable()
 export class MovementsService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async findAll() {
-    return await this.prismaService.movement.create({
-      data: {
-        balance: 0,
-        category: {
-          connectOrCreate: {
-            create: {
-              name: 'Sin Categoria',
+  async findAll(listMovemtsDto: ListMovemtsDto) {
+    const { page = 1, size = 25 } = listMovemtsDto
+
+    const [count, data] = await this.prismaService.$transaction([
+      this.prismaService.movement.count(),
+      this.prismaService.movement.findMany({
+        skip: (page - 1) * size,
+        take: size,
+        orderBy: [{ createdAt: 'desc' }, { updatedAt: 'desc' }],
+        include: {
+          product: {
+            omit: {
+              categoryId: true,
+              typeProductId: true,
+              typePresentationId: true,
+              updatedAt: true,
+              createdAt: true,
             },
-            where: {
-              name: 'Sin Categoria',
+          },
+          category: {
+            select: {
+              name: true,
+              id: true,
+            },
+          },
+          moventType: {
+            select: {
+              name: true,
+              id: true,
+            },
+          },
+          TypePresentation: {
+            select: { id: true, name: true },
+          },
+          typeProduct: {
+            select: {
+              id: true,
+              name: true,
             },
           },
         },
-        date: new Date(),
-        moventType: {
-          connectOrCreate: {
-            create: {
-              name: 'ENTRADA',
-            },
-            where: {
-              name: 'ENTRADA',
-            },
-          },
+        omit: {
+          productId: true,
+          categoryId: true,
+          movementTypeId: true,
+          typePresentationId: true,
+          typeProductId: true,
         },
-        product: {
-          connect: {
-            id: '1b8f64c1-2d31-4e04-9916-41018191fe6b',
-          },
-        },
-        entry: 2,
-        exit: 2,
-        TypePresentation: {
-          connectOrCreate: {
-            create: {
-              name: 'presentation',
-            },
-            where: {
-              name: 'presentation',
-            },
-          },
-        },
-        typeProduct: {
-          connectOrCreate: {
-            create: {
-              name: 'nohay',
-            },
-            where: {
-              name: 'nohay',
-            },
-          },
-        },
-      },
-    })
+      }),
+    ])
+    if (!data || data.length === 0)
+      return {
+        data,
+        status: HttpStatus.OK,
+        count: 0,
+      }
+    return {
+      status: HttpStatus.OK,
+      count,
+      data,
+    }
   }
 }
