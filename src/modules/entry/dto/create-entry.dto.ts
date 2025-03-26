bb36af57db7bc255c09bb70e46fca6f990c40693
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-26 00:16:39 -0500

    update create-entry.dto.ts

--- src/modules/entry/dto/create-entry.dto.ts
+++ src/modules/entry/dto/create-entry.dto.ts
@@ -1,1 +1,34 @@
-export class CreateEntryDto {}
+import { Transform } from 'class-transformer'
+import { IsDate, IsInt, IsOptional, IsString } from 'class-validator'
+export class CreateEntryDto {
+  @IsOptional()
+  @IsDate()
+  date?: Date
+
+  @IsInt()
+  @Transform(({ value }) => parseInt(value))
+  entry: number
+  
+  @IsInt()
+  @Transform(({ value }) => parseInt(value))
+  balance: number
+
+  @IsString()
+  movementTypeId: string
+
+  @IsOptional()
+  @IsString()
+  description?: string
+
+  @IsString()
+  categoryId: string
+
+  @IsString()
+  productId: string
+
+  @IsString()
+  typeProductId: string
+
+  @IsString()
+  typePresentationId: string
+}
