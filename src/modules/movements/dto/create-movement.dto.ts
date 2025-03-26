627d4d05265cd0b6e331edb1f199c8840c0b00a0
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-26 00:17:21 -0500

    update create-movement.dto.ts

--- src/modules/movements/dto/create-movement.dto.ts
+++ src/modules/movements/dto/create-movement.dto.ts
@@ -1,1 +1,38 @@
-export class CreateMovementDto {}
+import { IsDate, IsInt, IsOptional, IsString } from 'class-validator'
+
+export class CreateMovementDto {
+  @IsOptional()
+  @IsDate()
+  date?: Date
+
+  @IsOptional()
+  @IsInt()
+  entry?: number
+
+  @IsOptional()
+  @IsInt()
+  exit?: number
+
+  @IsOptional()
+  @IsInt()
+  balance: number
+
+  @IsOptional()
+  @IsString()
+  description?: string
+
+  @IsString()
+  productId: string
+
+  @IsString()
+  movementTypeId: string
+
+  @IsString()
+  typePresentationId: string
+
+  @IsString()
+  categoryId: string
+
+  @IsString()
+  typeProductId: string
+}
