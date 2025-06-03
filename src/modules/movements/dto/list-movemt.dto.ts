69ffd0a6ee20ee37a6ec3fa023bec68d64cc730b
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-03 02:10:48 -0500

    refactor: enhance ListMovemtsDto with additional optional fields and validation

--- src/modules/movements/dto/list-movemt.dto.ts
+++ src/modules/movements/dto/list-movemt.dto.ts
@@ -1,5 +1,5 @@
 import { Transform } from 'class-transformer'
-import { IsInt, IsOptional, Min } from 'class-validator'
+import { IsInt, IsOptional, IsString, Matches, Min } from 'class-validator'
 
 export class ListMovemtsDto {
   @IsOptional()
@@ -7,9 +7,26 @@ export class ListMovemtsDto {
   @IsInt()
   @Min(0)
   page: number = 1
+
   @IsOptional()
   @Transform(({ value }) => parseInt(value))
   @IsInt()
   @Min(0)
-  size: number = 25
+  size?: number = 25
+
+  @IsOptional()
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+    message: 'startDate debe ser en el formato YYYY-MM-DD',
+  })
+  startDate?: string
+
+  @IsOptional()
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+    message: 'endDate debe ser en el formato YYYY-MM-DD',
+  })
+  endDate?: string
+
+  @IsOptional()
+  @IsString()
+  search?: string
 }
